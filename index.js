5c410c259b4626b2c730814a2c98323b0f677e2c
venkat2305 <heliobvsr2002@gmail.com>
2023-08-01 23:14:18 +0530

    prettier code format

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const notifyBtn = document.getElementById("notifybtn")
-const error1 = document.getElementById("error1")
-const error2 = document.getElementById("error2")
-const emailInput = document.getElementById("emailinput")
+const notifyBtn = document.getElementById("notifybtn");
+const error1 = document.getElementById("error1");
+const error2 = document.getElementById("error2");
+const emailInput = document.getElementById("emailinput");
 
-function isValidEmail(email){
+function isValidEmail(email) {}
 
-}
-
-notifyBtn.addEventListener("click", ()=>{
-    input = emailInput.value
-    if(input == ""){
-        error1.style.display = "block"
-    }else if(!/\S+@\S+\.\S+/.test(input) ){
-        error2.style.display = "block"
-    }else if(/\S+@\S+\.\S+/.test(input)){
-        alert("Thank you for subscribing")
-    }
-    
-})
+notifyBtn.addEventListener("click", () => {
+  input = emailInput.value;
+  if (input == "") {
+    error1.style.display = "block";
+  } else if (!/\S+@\S+\.\S+/.test(input)) {
+    error2.style.display = "block";
+  } else if (/\S+@\S+\.\S+/.test(input)) {
+    alert("Thank you for subscribing");
+  }
+});

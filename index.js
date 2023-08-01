5ff497d15c5fb6fd90d48b9ad84b503cd1bdc064
venkat2305 <heliobvsr2002@gmail.com>
2023-08-01 16:33:40 +0530

    js done

---
+++ index.js
@@ -0,0 +1,18 @@
+const notifyBtn = document.getElementById("notifybtn")
+const error1 = document.getElementById("error1")
+const error2 = document.getElementById("error2")
+const emailInput = document.getElementById("emailinput")
+
+function isValidEmail(email){
+
+}
+
+notifyBtn.addEventListener("click", ()=>{
+    input = emailInput.value
+    if(input == ""){
+        error1.style.display = "block"
+    }else if(!/\S+@\S+\.\S+/.test(input) ){
+        error2.style.display = "block"
+    }
+    
+})
